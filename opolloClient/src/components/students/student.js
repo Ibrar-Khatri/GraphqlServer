bd11f53d0f4aad7fe5ffd545ce2d69068c9c7b77
Ibrar-Khatri <khatriibrar116@gmail.com>
2021-10-09 07:44:44 +0500

    mutations and query work completed

---
+++ opolloClient/src/components/students/student.js
@@ -0,0 +1,154 @@
+import React, { useState } from "react";
+import { useQuery, gql, useMutation } from "@apollo/client";
+import "./student.css";
+
+const getAllStudents = gql`
+	query GetAlStudents {
+		students {
+			name
+			age
+			id
+		}
+	}
+`;
+const addStudent = gql`
+	mutation AddStudent($name: String, $age: Int, $id: Int) {
+		addStudent(input: { name: $name, age: $age, id: $id }) {
+			name
+			age
+			id
+		}
+	}
+`;
+const updateStudent = gql`
+	mutation UpdateStudent($name: String, $age: Int, $id: Int) {
+		updateStudent(input: { name: $name, age: $age, id: $id }) {
+			name
+			age
+			id
+		}
+	}
+`;
+const deleteStudentById = gql`
+	mutation DeleteStudent($id: Int) {
+		deleteStudent(input: { id: $id }) {
+			name
+			age
+			id
+		}
+	}
+`;
+
+function StudentList() {
+	const [addStu] = useMutation(addStudent);
+	const [updateStu] = useMutation(updateStudent);
+	const [deleteStu] = useMutation(deleteStudentById);
+	let queryData = useQuery(getAllStudents);
+	console.log(queryData?.data?.students, "data");
+
+	let [name, setName] = useState("");
+	let [age, setAge] = useState();
+	let [id, setId] = useState();
+	let [isEdit, setIsEdit] = useState(false);
+
+	if (queryData.loading) return <h1>loading</h1>;
+	if (queryData.error) return <h1>{queryData.error}</h1>;
+	let { students } = queryData.data;
+
+	function addAndUpdate(e) {
+		e.preventDefault();
+		if (!isEdit) {
+			addStu({
+				variables: {
+					name: name,
+					age: Number(age),
+					id: students.length + 1,
+				},
+			});
+		} else {
+			updateStu({
+				variables: {
+					name: name,
+					age: Number(age),
+					id: id,
+				},
+			});
+		}
+		setName("");
+		setAge("");
+		setId();
+		setIsEdit(false);
+	}
+
+	function deleteStudent(id) {
+		deleteStu({
+			variables: {
+				id: id,
+			},
+		});
+	}
+
+	return (
+		<div>
+			<form className="form" onSubmit={addAndUpdate}>
+				<input
+					type="text"
+					name="name"
+					value={name}
+					onChange={(text) => setName(text.target.value)}
+					required
+				/>
+				<input
+					type="number"
+					name="age"
+					value={age}
+					onChange={(text) => setAge(text.target.value)}
+					required
+				/>
+				{isEdit ? (
+					<button type="submit">Edit</button>
+				) : (
+					<button type="submit">Add</button>
+				)}
+			</form>
+			<h1>Student List</h1>
+			<table>
+				<thead>
+					<tr>
+						<th>Id</th>
+						<th>Name</th>
+						<th>Age</th>
+					</tr>
+				</thead>
+				<tbody>
+					{students?.map((item, i) => (
+						<tr key={i}>
+							<td>{item.id}</td>
+							<td>{item.name}</td>
+							<td>{item.age}</td>
+							<td>
+								<button
+									onClick={() => {
+										setName(item.name);
+										setAge(item.age);
+										setId(item.id);
+										setIsEdit(true);
+									}}
+								>
+									edit
+								</button>
+							</td>
+							<td>
+								<button type="submit" onClick={() => deleteStudent(item.id)}>
+									Delete
+								</button>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
+}
+
+export default StudentList;
--- oppoloServer/index.js
+++ oppoloServer/index.js
@@ -1,10 +1,18 @@
 const { ApolloServer, gql } = require("apollo-server");
+const { createServer } = require("http");
+const { SubscriptionServer } = require("subscriptions-transport-ws");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { execute, subscribe } = require("graphql");
+const express = require("express");
+const app = express();
+
+const httpServer = createServer(app);
 
 const students = [
-	{ name: "Ali", email: "ali@gmail.com", age: 20, id: 1 },
-	{ name: "Ahmed", email: "Ahmed@gmail.com", age: 25, id: 2 },
-	{ name: "Mubashir", email: "mubashir@gmail.com", age: 30, id: 3 },
-	{ name: "Raza", email: "raza@gmail.com", age: 35, id: 4 },
+	{ name: "Ali", age: 20, id: 1 },
+	{ name: "Ahmed", age: 25, id: 2 },
+	{ name: "Mubashir", age: 30, id: 3 },
+	{ name: "Raza", age: 35, id: 4 },
 ];
 
 const resolvers = {
@@ -13,8 +21,27 @@ const resolvers = {
 	},
 	Mutation: {
 		addStudent: (e, { input }) => {
-			console.log(input);
 			students.push(input);
+			return input;
+		},
+		updateStudent: (e, { input }) => {
+			students.forEach((stu) => {
+				if (stu.id === input.id) {
+					stu.name = input.name;
+					stu.age = input.age;
+				}
+			});
+			return input;
+		},
+		deleteStudent: (e, { input }) => {
+			console.log(input.id);
+			// let abc = students.filter((stu) => stu.id != input.id);
+			students.forEach((stu, i) => {
+				if (stu.id === input.id) {
+					students.splice(i, 1);
+				}
+			});
+
 			return input;
 		},
 	},
@@ -24,14 +51,22 @@ const typeDefs = gql`
 	type Student {
 		id: Int
 		name: String
-		email: String
 		age: Int
 	}
 
 	input stInput {
 		id: Int
 		name: String
-		email: String
+		age: Int
+	}
+	input updateStu {
+		id: Int
+		name: String
+		age: Int
+	}
+	input deleteStu {
+		id: Int
+		name: String
 		age: Int
 	}
 
@@ -41,12 +76,49 @@ const typeDefs = gql`
 
 	type Mutation {
 		addStudent(input: stInput): Student
+		updateStudent(input: updateStu): Student
+		deleteStudent(input: deleteStu): Student
 	}
 `;
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+const server = new ApolloServer({
+	schema,
+	plugins: [
+		{
+			async serverWillStart() {
+				return {
+					async drainServer() {
+						subscriptionServer.close();
+					},
+				};
+			},
+		},
+	],
+});
+const subscriptionServer = SubscriptionServer.create(
+	{
+		// This is the `schema` we just created.
+		schema,
+		// These are imported from `graphql`.
+		execute,
+		subscribe,
+	},
+	{
+		// This is the `httpServer` we created in a previous step.
+		server: httpServer,
+		// This `server` is the instance returned from `new ApolloServer`.
+		path: server.graphqlPath,
+	},
+);
 
 // The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-	console.log(`🚀  Server ready at ${url}`);
+// server.listen().then(({ url }) => {
+// 	console.log(`🚀  Server ready at ${url}`);
+// });
+httpServer.listen(5000, () => {
+	console.log("Server started successfully˝");
 });
+
+//how to create opollo server difined easily this youtuber Cory McAboy
